d7932a0cd73dcf8872281392654c52b0008411ea
mathbueeno <mathbueeno@gmail.com>
2022-11-01 18:07:18 -0300

    Update Contatos

---
+++ SistemaDeCadastros/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿]using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDeCadastros.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
